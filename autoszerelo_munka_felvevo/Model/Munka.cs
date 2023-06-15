103ef07a49d850f401f113795e2ad23023364dec
kmironp <99607766+kmironp@users.noreply.github.com>
2023-06-15 16:02:44 +0200

    Update Munka.cs
    Munka felvevő kliens - Blazor WebAssembly alkalmazás Munkafelvételhez szükséges mezők új leírás alapján elkészült változata a feltételekkel.

--- autoszerelo_munka_felvevo/Model/Munka.cs
+++ autoszerelo_munka_felvevo/Model/Munka.cs
@@ -1,27 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace autoszerelo_munka_felvevo.Model
 {
     public class Munka
     {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
-        [Required(ErrorMessage = "Kötelező mező!")]
-        public string Nev { get; set; }
-        [Required(ErrorMessage = "Kötelező mező!")]
-        public string Tipus { get; set; }
-        [Required(ErrorMessage = "Kötelező mező!")]
-        public string Rendszam { get; set; }
-        [Required(ErrorMessage = "Kötelező mező!")]
-        public int Ev { get; set; }
-        [Required(ErrorMessage = "Kötelező mező!")]
-        public string Kategoria { get; set; }
-        [Required(ErrorMessage = "Kötelező mező!")]
-        public string Leiras { get; set; }
-        [Required(ErrorMessage = "Kötelező mező!")]
-        [Range(0, 10, ErrorMessage = "Ezen mező értéke 1 és 10 között kell, hogy legyen!")]
-        public int Sulyossag { get; set; }
+        [Required(ErrorMessage = "Ügyfél neve kötelező.")]
+        [RegularExpression(@"^[^\s!?_\-:;#]+$", ErrorMessage = "Ügyfél név nem lehet üres és nem tartalmazhat speciális karaktereket.")]
+        public string UgyfelNeve { get; set; }
+
+        [Required(ErrorMessage = "Autó típusa és rendszáma kötelező.")]
+        [RegularExpression(@"^(?!.*[\s!?_\-:;#])^[A-Z]{3}-\d{3}$", ErrorMessage = "Autó típusa és rendszáma nem lehet üres és nem tartalmazhat speciális karaktereket. Rendszám formátuma: XXX-000")]
+        public string AutoTipusRendszam { get; set; }
+
+        [Required(ErrorMessage = "Gyártási év kötelező.")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Gyártási év csak számokat tartalmazhat.")]
+        public int GyartasiEv { get; set; }
+
+        [Required(ErrorMessage = "Munka kategória kötelező.")]
+        [RegularExpression(@"^(Karosszéria|motor|futómű|fékberendezés)$", ErrorMessage = "Érvénytelen munka kategória.")]
+        public string MunkaKategoria { get; set; }
+
+        [Required(ErrorMessage = "Az autó hibájának leírása kötelező.")]
+        public string HibaLeiras { get; set; }
+
+        [Required(ErrorMessage = "A hiba súlyossága kötelező.")]
+        [Range(1, 10, ErrorMessage = "A hiba súlyossága 1 és 10 közötti szám lehet.")]
+        public int HibaSulyossaga { get; set; }
     }
 }
